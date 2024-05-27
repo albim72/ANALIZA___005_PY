acfc6954326e572c94526d5fa7d8175caf4aab94
albim72 <marcin.albiniak72@gmail.com>
2024-05-27 14:08:51 +0200

    Create main.py

---
+++ DZIEN_1/moje_moduly/main.py
@@ -0,0 +1,14 @@
+# import dane
+# import dane as dn
+from dane import nb as filia
+from dane import book as bk
+from funkcje.bfunkcje import czytaj_liste
+from funkcje.bfunkcje import czytaj_slownik
+
+print("_____________ wyświetlenie kolekcji ___________")
+print(filia)
+print(bk)
+
+print("_____________ wyświetlenie kolekcji ___________")
+czytaj_liste(filia)
+czytaj_slownik(bk)
