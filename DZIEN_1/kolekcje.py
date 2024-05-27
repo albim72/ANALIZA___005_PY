7960f6e6793d49a5909768a52ceddad3d0efd88e
albim72 <marcin.albiniak72@gmail.com>
2024-05-27 11:03:53 +0200

    Update kolekcje.py

--- DZIEN_1/kolekcje.py
+++ DZIEN_1/kolekcje.py
@@ -16,3 +16,35 @@ print(cyfry[0])
 print(cyfry[2])
 print(cyfry[2:5])
 print(cyfry[-1])
+
+cyfry.append(1)
+print(cyfry)
+cyfry.insert(3,1)
+print(cyfry)
+ekstra = [5,5,5,5,5,5,5]
+
+cyfry.extend(ekstra)
+print(cyfry)
+
+del cyfry[12]
+print(cyfry)
+cyfry.remove(7)
+print(cyfry)
+
+cyfry.sort()
+print(cyfry)
+
+cyfry = cyfry + [6,1,1,1,2,7,9,5]
+#konkatenacja list
+print(cyfry)
+
+imiona = ["Jan","Anna","Daniel"]
+imiona = 4*imiona
+print(imiona)
+
+t = "lajkonik"
+print(t)
+print(t[0])
+print(t[4])
+print(t[2:6])
+print(t[-1])
