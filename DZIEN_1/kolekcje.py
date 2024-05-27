d10a95975d2fbf4ef4e0308519dadbe7143089f8
albim72 <marcin.albiniak72@gmail.com>
2024-05-27 10:29:57 +0200

    Create kolekcje.py

---
+++ DZIEN_1/kolekcje.py
@@ -0,0 +1,18 @@
+print("Kolekcje języka Python!")
+
+a:int=700
+print(a)
+print(type(a))
+
+a = "siedemset"
+print(a)
+print(type(a))
+
+#lista
+cyfry = [2,6,7,4,8,9,2,4,2,4,5,1,5,8,5,3,2,5,7,8,6,4,6]
+print(cyfry)
+print(type(cyfry))
+print(cyfry[0])
+print(cyfry[2])
+print(cyfry[2:5])
+print(cyfry[-1])
