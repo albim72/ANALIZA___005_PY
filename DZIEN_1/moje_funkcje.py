d143bb6a21bce8fc7bd9a153207e5624cd01c9fa
albim72 <marcin.albiniak72@gmail.com>
2024-05-27 13:11:45 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -1,8 +1,12 @@
 #przykład 1
+
+def nk(k):
+    return k**0.5+k
+
 n = 100
 def policz(a,b,c,y=6):
     global n
-    n = (a+b)*y-c*n
+    n = (a+b)*y-c*n*nk(b)
     return n
 
 print(policz(45,6,78,9))
@@ -10,3 +14,14 @@ print(policz(-0.545,-10,5.78,11))
 print(policz(2,3,5.66))
 
 print(n)
+
+#przykład 2
+
+def rank(*lang,nrrank,**inne):
+    print(f"ranking języków programowania nr {nrrank} -> 1. {lang[0]}, 2. {lang[1]}. 3. {lang[2]}")
+
+rank("Python","Java","C#","JavaScript",nrrank=56)
+rank("Python","C++","Java","C#","JavaScript","TypeScript",nrrank=56)
+
+
+
