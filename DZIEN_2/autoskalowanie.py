c854d585e6b489705e49a76b2dc492598b501113
albim72 <marcin.albiniak72@gmail.com>
2024-05-28 11:39:49 +0200

    Update autoskalowanie.py

--- DZIEN_2/autoskalowanie.py
+++ DZIEN_2/autoskalowanie.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(-2*np.pi,2*np.pi,100)
 y = np.sinc(x)
+
+
+fig,ax = plt.subplots(ncols=2,figsize=(12,8))
+ax[0].plot(x,y)
+ax[0].set_title('pojedyncza krzywa')
+ax[1].plot(x,y)
+ax[1].plot(x*2.0,y)
+ax[1].set_title('dwie krzywe')
+plt.show()
